ca4d6bd7171768ee20fa6447576fbce0d49e072a
Marcos Benevides <marcosbenevides@outlook.com.br>
2016-05-11 16:04:28 -0300

    Versão 1.0 -Adicionado eHit() modificado pelo Mike -Adicionado listaLRU para controle do tipo de memoria LRU

---
+++ src/com/una/simulador/Bloco.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.una.simulador;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Bloco extends MemoriaCache {
+
+    private Boolean bitValidade;
+    private String historico[][];
+    private Integer id, contHistorico = 0, contPalavra = 0, qntPalavra;
+
+    public Bloco(Integer qntPalavra, String tipoMemoria, String politicaSubs, Integer mT) {
+
+        MemoriaCache.setTipoCache(tipoMemoria);
+        MemoriaCache.setPoliticaSubs(politicaSubs);
+
+        Bloco.criarListaLRU(mT * 2);
+
+        historico = new String[mT][qntPalavra];
+        bitValidade = false;
+        this.qntPalavra = qntPalavra;
+        this.setId();
+    }
+
+    public void setHistorico(String palavra, String[] p) {
+        if (getBitValidade()) {
+            if (eHit(palavra, getContHistorico())) {
+                setHit();
+            } else if (qntPalavra > 1) {
+                for (int i = 0; i < p.length; i++) {
+                    historico[i][getContHistorico()] = p[i];
+                    setMiss();
+                }
+                setContHistorico();
+
+            } else {
+                historico[0][getContHistorico()] = palavra;
+                setContHistorico();
+                setMiss();
+            }
+
+        } else {
+            setMissComp();
+            setBitValidade((Boolean) true);
+            setHistorico(palavra, p);
+        }
+    }
+
+    public Boolean eHit(String palavra, Integer numCol) {
+        System.out.println(palavra + "\t" + numCol + "\t" + historico.length
+                + "\t" + historico[0].length);
+        if (!getBitValidade()) {
+            return false;
+        } else {
+
+            for (int i = 0; i < 1; i++) {
+                for (int j = 0; j <= numCol; j++) {
+                    if (historico[i][j] == null) {
+                        return false;
+                    } else if (historico[i][j].isEmpty()) {
+                        return false;
+                    } else {
+                        for (int x = i; x < historico.length; x++) {
+                            if (historico[x][j].equalsIgnoreCase(palavra)) {
+                                return true;
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public Boolean eHistoricoVazio() {
+        Integer vazio = 0, cheio = 0;
+        Boolean e = false;
+        String h;
+        for (String[] historico1 : historico) {
+            for (String historico11 : historico1) {
+                if (historico11.isEmpty()) {
+                    vazio++;
+                } else {
+                    cheio++;
+                }
+            }
+        }
+        if (vazio == historico.length) {
+            e = true;
+        } else if (cheio > 0) {
+            e = false;
+        }
+        return e;
+    }
+
+    /**
+     * @return the bitValidade
+     */
+    public Boolean getBitValidade() {
+        return bitValidade;
+    }
+
+    /**
+     * @param bitValidade the bitValidade to set
+     */
+    public void setBitValidade(Boolean bitValidade) {
+        this.bitValidade = bitValidade;
+    }
+
+    /**
+     * @return the historico
+     */
+    public String getHistorico() {
+        String hist = "";
+        for (String[] historico1 : historico) {
+            for (String historico11 : historico1) {
+                hist += "[" + historico11 + "]";
+            }
+            hist += "\n";
+        }
+        return hist;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    private void setId() {
+        this.id = getEndereco();
+    }
+
+    /**
+     * @return the contHistorico
+     */
+    public Integer getContHistorico() {
+        return contHistorico;
+    }
+
+    /**
+     */
+    public void setContHistorico() {
+        this.contHistorico++;
+    }
+
+    @Override
+    public String toString() {
+        return "Bloco de memória cache \tEndereço: " + this.getId()
+                + "\nBit Validade: " + getBitValidade() + "\nHistórico do bloco:\n"
+                + getHistorico() + "\n-----------------------\n";
+    }
+
+    public String toString2() {
+        return super.toString();
+    }
+
+    /**
+     * @return the contPalavra
+     */
+    public Integer getContPalavra() {
+        return contPalavra;
+    }
+
+    /**
+     * @param contPalavra the contPalavra to set
+     */
+    public void setContPalavra(Integer contPalavra) {
+        this.contPalavra++;
+    }
+
+}
---
+++ src/com/una/simulador/MemoriaCache.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.una.simulador;
+
+/**
+ *
+ * @author Marcos
+ */
+public abstract class MemoriaCache {
+
+    static private Integer hit = 0, missComp = 0, miss = 0,
+            endereco = 0, tempoMedio = 0, controleLista = 0, posicaoLista;
+    static private String politicaSubs, tipoCache, listaLRU[];
+
+    /**
+     * @return the politicaSubs
+     */
+    public static String getPoliticaSubs() {
+        return politicaSubs;
+    }
+
+    /**
+     * @param aPoliticaSubs the politicaSubs to set
+     */
+    public static void setPoliticaSubs(String aPoliticaSubs) {
+        politicaSubs = aPoliticaSubs;
+    }
+
+    /**
+     * @return the tipoCache
+     */
+    public static String getTipoCache() {
+        return tipoCache;
+    }
+
+    /**
+     * @param aTipoCache the tipoCache to set
+     */
+    public static void setTipoCache(String aTipoCache) {
+        tipoCache = aTipoCache;
+    }
+
+    /**
+     * @return the controleLista
+     */
+    public static Integer getControleLista() {
+        return controleLista;
+    }
+
+    /**
+     * @param aControleLista the controleLista to set
+     */
+    public static void setControleLista() {
+        controleLista++;
+    }
+
+    public static void criarListaLRU(Integer valor) {
+        listaLRU = new String[valor];
+    }
+
+    /**
+     * @return the listaLRU
+     */
+    public static String getListaLRU() {
+        String lista = "";
+        for (String lista1 : listaLRU) {
+            lista += "[" + lista1 + "]";
+        }
+        return lista;
+    }
+
+    /**
+     * @param numBin
+     */
+    public static void setListaLRU(String numBin) {
+        if (getControleLista().equals(0)) {
+            listaLRU[getControleLista()] = numBin;
+            setControleLista();
+        } else if (taNaLista(numBin)) {
+            listaLRU[getControleLista()] = listaLRU[posicaoLista];
+            posicaoLista = null;
+            setControleLista();
+        }
+    }
+
+    private static boolean taNaLista(String numBin) {
+        for (int i = 0; i < getControleLista(); i++) {
+            if (listaLRU[i].equalsIgnoreCase(numBin)) {
+                posicaoLista = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean eListaLRUVazia() {
+        for (String listaLRU1 : listaLRU) {
+            try {
+                if (listaLRU1 == null || listaLRU1.isEmpty()) {
+                    System.out.println("-- vazio --");
+                } else {
+                    return false;
+                }
+            } catch (NullPointerException ex) {
+                System.err.println("Erro eListaLRUVazia " + ex);
+            }
+        }
+        return true;
+    }
+
+    public MemoriaCache() {
+        MemoriaCache.setEndereco();
+    }
+
+    /**
+     * @return the hit
+     */
+    public static Integer getHit() {
+        return hit;
+    }
+
+    /**
+     */
+    public static void setHit() {
+        hit++;
+    }
+
+    /**
+     * @return the missComp
+     */
+    public static Integer getMissComp() {
+        return missComp;
+    }
+
+    /**
+     */
+    public static void setMissComp() {
+        missComp++;
+    }
+
+    /**
+     * @return the miss
+     */
+    public static Integer getMiss() {
+        return miss;
+    }
+
+    /**
+     */
+    public static void setMiss() {
+        miss++;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public static Integer getEndereco() {
+        return endereco;
+    }
+
+    /**
+     */
+    public static void setEndereco() {
+        endereco++;
+    }
+
+    @Override
+    public String toString() {
+        return "Hits: " + getHit() + "\tMiss Compusórios: " + getMissComp() + "\tMiss: " + getMiss();
+    }
+
+    /**
+     *
+     * @param tempoCache
+     * @param ram
+     * @return
+     */
+    protected static Integer getTempoMedio(Integer tempoCache, Integer ram) {
+        return (tempoCache + ((1 - getHit()) * ram));
+    }
+}
